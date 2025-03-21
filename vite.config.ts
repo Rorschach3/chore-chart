dd9bdfb89d91c29445f73864d7769b858a861250
Daniel Hernandez <daniel_fhernandez@yahoo.com>
2025-03-21 10:21:58 -0700

    updated allowedHosts on viteconfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "980169ec-b704-4303-a6ce-f36f12bad555.lovableproject.com"
+    ]
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
