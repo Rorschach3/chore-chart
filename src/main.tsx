e5d9e2f656eeba0dcfb293e32e8ca8c3587be3e1
rorschach3 <daniel_fhernandez@yahoo.com>
2025-03-21 22:42:21 -0700

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+        <Analytics />
+    </React.StrictMode>
+);
